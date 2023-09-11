0bd1150cbebf7a3463318ec699fdb3cdd0a9b276
Nikhil Nigade <nikhil@dezinezync.com>
2023-09-11 07:12:09 +0530

    dynamic lib
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

--- Package.swift
+++ Package.swift
@@ -4,28 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "Puppy",
-    platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
-    ],
-    products: [
-        .library(name: "Puppy", targets: ["Puppy"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.5.2")),
-    ],
-    targets: [
-        .target(name: "CPuppy",
-                exclude: ["CMakeLists.txt"]),
-        .target(name: "Puppy", dependencies: [.product(name: "Logging", package: "swift-log")],
-                exclude: ["CMakeLists.txt"]),
-        .testTarget(name: "PuppyTests", dependencies: ["Puppy"]),
-    ],
-    swiftLanguageVersions: [.v5]
+  name: "Puppy",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
+  ],
+  products: [
+    .library(name: "Puppy", type: .dynamic, targets: ["Puppy"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.5.2")),
+  ],
+  targets: [
+    .target(name: "CPuppy",
+            exclude: ["CMakeLists.txt"]),
+    .target(name: "Puppy", dependencies: [.product(name: "Logging", package: "swift-log")],
+            exclude: ["CMakeLists.txt"]),
+    .testTarget(name: "PuppyTests", dependencies: ["Puppy"]),
+  ],
+  swiftLanguageVersions: [.v5]
 )
 
 if let puppy = package.targets.first(where: { $0.name == "Puppy" }) {
-    puppy.dependencies.append(
-        .target(name: "CPuppy", condition: .when(platforms: [.linux]))
-    )
+  puppy.dependencies.append(
+    .target(name: "CPuppy", condition: .when(platforms: [.linux]))
+  )
 }
