08b2ef5fa2bb231ca7747e61064c1678e9b844e5
Nikhil Nigade <nikhil@dezinezync.com>
2026-04-26 17:19:09 +0530

    raise swift-log dep
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     .library(name: "Puppy", type: .static, targets: ["Puppy"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.6.3")),
+    .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.12.0")),
   ],
   targets: [
     .target(
