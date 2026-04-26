2213147574399a6fa095d701428fab5e1a6c5671
Nikhil Nigade <nikhil@dezinezync.com>
2026-04-26 16:28:53 +0530

    fix package, raise tooling to 6.0
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

--- Package.swift
+++ Package.swift
@@ -1,37 +1,36 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Puppy",
-    platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1),
-    ],
-    products: [
-      .library(name: "Puppy", type: .static, targets: ["Puppy"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.6.3")),
-    ],
-    targets: [
-        .target(
-            name: "CPuppy",
-            path: "Sources/CPuppy",
-            exclude: ["CMakeLists.txt"]
-        ),
-        .target(
-            name: "Puppy",
-            dependencies: [.product(name: "Logging", package: "swift-log")],
-            path: "Sources/Puppy",
-            exclude: ["CMakeLists.txt"]
-        ),
-        .testTarget(
-            name: "PuppyTests",
-            dependencies: ["Puppy"],
-            path: "Tests/PuppyTests"
-        ),
-    ],
+  name: "Puppy",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1),
+  ],
+  products: [
+    .library(name: "Puppy", type: .static, targets: ["Puppy"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.6.3")),
+  ],
+  targets: [
+    .target(
+      name: "CPuppy",
+      path: "Sources/CPuppy",
+      exclude: ["CMakeLists.txt"]
+    ),
+    .target(
+      name: "Puppy",
+      dependencies: [.product(name: "Logging", package: "swift-log")],
+      path: "Sources/Puppy",
+      exclude: ["CMakeLists.txt"]
+    ),
+    .testTarget(
+      name: "PuppyTests",
+      dependencies: ["Puppy"],
+      path: "Tests/PuppyTests"
+    ),
   ]
 )
 
